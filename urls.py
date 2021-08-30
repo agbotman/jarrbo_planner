cdcb62a2c537eaa60a31bfe898406b01a5a1e813
Ton Botman <ton@jarrbo.nl>
2021-08-30 20:26:55 +0200

    updated url and views

--- urls.py
+++ urls.py
@@ -1,5 +1,8 @@
 from django.conf.urls import include, url
+from .views import jarrbo_planner, jarrbo_planner_two, jarrbo_planner_three
 
 urlpatterns = [
-    url(r'^$', jarrbo_home, name='jarrbo_home'),
+    url(r'^$', jarrbo_planner, name='jarrbo_planner'),
+    url(r'^two/$', jarrbo_planner_two, name='jarrbo_planner_two'),
+    url(r'^three/$', jarrbo_planner_three, name='jarrbo_planner_three'),
 ]
--- views.py
+++ views.py
@@ -3,3 +3,8 @@ from django.shortcuts import render
 def jarrbo_planner(request):
     return render(request, 'jarrbo_planner.html',)
     
+def jarrbo_planner_two(request):
+    return render(request, 'jarrbo_planner_two.html',)
+
+def jarrbo_planner_three(request):
+    return render(request, 'jarrbo_planner_three.html',)
